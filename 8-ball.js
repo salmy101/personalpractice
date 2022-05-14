69ed7d56f9a5fc6d251dacfbd7db5f030b1e8702
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-05-14 21:52:49 +0000

    my attempt to make an 8-ball with a switch statement

---
+++ 8-ball.js
@@ -0,0 +1,22 @@
+
+const eightBall = function(num) {
+  return Math.floor(Math.random());
+}
+switch (eightBall) {
+  case 1:
+    console.log("the answer is within yourself.")
+    break;
+  case 2:
+    console.log("the answer you seek is far out of reach")
+    break; 
+  case 3:
+    console.log("All signs point towards yes")
+    break;
+  case 4:
+    console.log("Now is not the time t pursue such actions")
+    break;
+  case 5: 
+    console.log("I would hold off on that if i were you")
+    break;  
+}
+  
